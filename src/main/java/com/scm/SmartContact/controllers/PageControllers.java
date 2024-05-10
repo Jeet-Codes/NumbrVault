c8f7e8f34ddabd865123bdf9d5af8c7a1bac7fb2
Sandeep <sandeepkumar090304@gmail.com>
2024-05-10 23:14:50 +0530

    changed in File

--- src/main/java/com/scm/SmartContact/controllers/PageControllers.java
+++ src/main/java/com/scm/SmartContact/controllers/PageControllers.java
@@ -1,4 +1,4 @@
-package com.scm.controllers;
+package com.scm.SmartContact.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
