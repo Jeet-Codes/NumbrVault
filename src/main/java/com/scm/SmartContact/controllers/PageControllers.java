a4502f7cb585b7f50b814c3dab6b844d00635815
Sandeep <sandeepkumar090304@gmail.com>
2024-05-11 23:32:39 +0530

    Data transferring

--- src/main/java/com/scm/SmartContact/controllers/PageControllers.java
+++ src/main/java/com/scm/SmartContact/controllers/PageControllers.java
@@ -1,15 +1,17 @@
 package com.scm.SmartContact.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageControllers {
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("Home Page Handler");
-		
+		model.addAttribute("name","Substring Technology");
+		model.addAttribute("Githubrepo","https://github.com/Jeet-Codes/NumbrVault");
 		return "home";
 	}
 }
