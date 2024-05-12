3bac5aace40e11fa0a27e5178e04560ac98bad57
Sandeep <sandeepkumar090304@gmail.com>
2024-05-12 23:54:53 +0530

    Added Tailwind CSS

--- src/main/java/com/scm/SmartContact/controllers/PageControllers.java
+++ src/main/java/com/scm/SmartContact/controllers/PageControllers.java
@@ -10,6 +10,8 @@ public class PageControllers {
 	@RequestMapping("/home")
 	public String home(Model model) {
 		System.out.println("Home Page Handler");
+
+		// Sending Data view
 		model.addAttribute("name","Substring Technology");
 		model.addAttribute("Githubrepo","https://github.com/Jeet-Codes/NumbrVault");
 		return "home";
